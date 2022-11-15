9382c744d5b3fc73f90b5864d308ec24de3f006e
ChezZak <115202136+ChezZak@users.noreply.github.com>
2022-11-15 11:52:22 +0700

    Create shutdown.js

---
+++ commands/shutdown.js
@@ -0,0 +1,36 @@
+const { exec } = require('child_process');
+const { MessageEmbed } = require("discord.js");
+const Discord = require('discord.js');
+module.exports = {
+name: "shutdown",
+description: "`Show detailed stats of bot`",
+category: "owner",
+aliases: ["off"],
+run: async (client, message, args, level) => {
+//command
+  if(message.author.id != UR_ID){
+    const noperms = new MessageEmbed()
+    .setDescription("You are not my owner!")
+    .setColor("YELLOW");
+    return message.channel.send(noperms)
+  } 
+
+const ls = exec('shutdown -h', function (stdout) {
+        console.log('Child Process STDOUT: ' + stdout);
+      });
+
+    let embed = new discord.MessageEmbed()
+    .setTitle(`Shutdown Your PC 📴`)
+    .setDescription(`• Successful shutdown your PC ✅`)
+    .setColor("RANDOM")
+      .setThumbnail(client.user.displayAvatarURL())
+      .setFooter(
+      `:>,
+      client.user.displayAvatarURL(),
+    );
+    
+    message.channel .send(embed)
+    
+  
+  }
+}
